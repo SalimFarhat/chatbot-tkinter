1703308d0e477b58144c7ef496e78263ef49815a
Salim Farhat <salimfarhat@gmail.com>
2021-09-18 02:47:23 -0400

    first commit

---
+++ NewPort/resources/javascript/javascript.js
@@ -0,0 +1,7 @@
+// Adding a comment
+const firstMessage = 'Hello World'
+console.log(firstMessage)
+const firstNumber = Math.floor(Math.random() * 5)
+console.log(firstNumber)
+console.log(firstMessage)
+// adding another comment
